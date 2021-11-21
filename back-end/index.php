5390983f43814ab4475fad6a65c990ece5fec92d
sandrosa1 <sandro.sa@fatec.sp.gov.br>
2021-11-21 08:54:31 -0300

    cria a rota no index

--- back-end/index.php
+++ back-end/index.php
@@ -12,6 +12,8 @@ include __DIR__ . '/routes/pages.php';
 
 // Inclui rotas do painel clientes APP
 include __DIR__ . '/routes/srv.php';
+// Inclui rotas do painel dos colaboradores racs
+include __DIR__ . '/routes/racs.php';
 
 // Imprime o response da rota
 $objRouter->run()->sendResponse();
