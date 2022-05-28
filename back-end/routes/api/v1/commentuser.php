23acbb77871d7fbffd3f8d87df80248554951165
Carolina Kaji <carolina.kaji@gmail.com>
2022-05-28 09:55:15 -0300

    Atualização

--- back-end/routes/api/v1/commentuser.php
+++ back-end/routes/api/v1/commentuser.php
@@ -1,48 +1,271 @@
 <?php
 
-use \App\Http\Response;
-use \App\Controller\Api;
+namespace App\Controller\Api;
 
-//Rota de consulta para trazer os comentários de um usuário 
-$objRouter->get('/api/v1/commentuser/',[
-    'middlewares' => [
-        'api',
-        'user-basic-auth' 
-    ],
-    function($request){
-        return new Response(200,Api\CommentUser::getCommentApp($request),'application/json');
-    }
-]);
+use \App\Model\Entity\User\Comment as EntityComments;
+use \App\Model\Entity\Aplication\App as EntityApps;
+use \SandroAmancio\PaginationManager\Pagination;
+use \App\Validate\Validate;
+use \App\Help\Help;
 
-//Rota de consulta para trazer os comentários de um usuário 
-$objRouter->get('/api/v1/commentuser/all',[
-    'middlewares' => [
-        'api',
-        'user-basic-auth' 
-    ],
-    function($request){
-        return new Response(200,Api\CommentUser::getCommentAppAll($request),'application/json');
-    }
-]);
+class CommentUser extends Api {
 
-//Rota de consulta para atualizar um comentário
-$objRouter->put('/api/v1/commentuser/{idComment}',[
-    'middlewares' => [
-        'api',
-        'user-basic-auth' 
-    ],
-    function($request, $idComment){
-        return new Response(200,Api\CommentUser::setEditCommentApp($request,$idComment),'application/json');
-    }
-]);
 
-//Rota de consulta para deletar um comentário
-$objRouter->delete('/api/v1/commentuser/{idComment}',[
-    'middlewares' => [
-        'api',
-        'user-basic-auth' 
-    ],
-    function($request, $idComment){
-        return new Response(200,Api\CommentUser::setDeleteCommentApp($request,$idComment),'application/json');
+    /**
+     * Método responsável por obter a renderização  dos itens de comentário para um usuário
+     * @param Request $request
+     *  @param Pagination $$objPagination
+     * @return string
+     */
+    private static function getAllCommentApp($request,$idUsuario,&$objPagination,$where){
+        //DEPOIMENTOS
+        $itens = [];
+
+        $queryParams = $request->getQueryParams();
+        $pagianaAtual = $queryParams['page'] ?? 1;
+        $filter = $queryParams['filter'] ?? "data";
+        $order = $queryParams['order'] ?? "DESC";
+
+        //QUANTIDADE TOTAL DE REGISTROS
+        $quatidadeTotal = EntityComments::getComment($where.' = '.$idUsuario, $filter.' '.$order ,null,'COUNT(*) as qtd')->fetchObject()->qtd;
+
+        //PAGINA ATUAL
+        $queryParams = $request->getQueryParams();
+        $pagianaAtual = $queryParams['page'] ?? 1;
+        
+        //INSTANCIA DE PAGINAÇÃO
+         $objPagination = new Pagination($quatidadeTotal,$pagianaAtual, 4);
+
+        //RESULTADOS DA PÁGINA
+        $results = EntityComments::getComment($where.' = '.$idUsuario,$filter.' '.$order,$objPagination->getLimit());
+
+        //RENDERIZA ITEM
+        while($objComment = $results->fetchObject(EntityComments::class)){
+        
+            $itens [] = [
+                'idComment'           => (int)$objComment->idComment,
+                'idApp'               => (int)$objComment->idApp,
+                'idUsuario'           => (int)$objComment->idUsuario,
+                'estabelecimento'     => $objComment->estabelecimento,
+                'nome'                => $objComment->nome,
+                'comentario'          => $objComment->comentario,
+                'utilSim'             => (int)$objComment->utilSim,
+                'utilNao'             => (int)$objComment->utilNao,
+                'data'                => $objComment->data,
+                'avaliacao'           => (int)$objComment->avaliacao,
+                'custo'               => (int)$objComment->custo,
+            ];
+            
+        }
+
+        //RETORNA OS DEPOIMENTOS
+        return $itens;
     }
-]);
+
+     /**
+     * Método responsável por obter a renderização  dos itens de comentário para um usuário
+     * @param Request $request
+     *  @param Pagination $$objPagination
+     * @return string
+     */
+    private static function getAllCommentAppAll($request,$idUsuario,$where){
+        //DEPOIMENTOS
+        $itens = [];
+
+        $queryParams = $request->getQueryParams();
+        $pagianaAtual = $queryParams['page'] ?? 1;
+        $filter = $queryParams['filter'] ?? "data";
+        $order = $queryParams['order'] ?? "DESC";
+
+        //QUANTIDADE TOTAL DE REGISTROS
+        $quatidadeTotal = EntityComments::getComment($where.' = '.$idUsuario, $filter.' '.$order ,null,'COUNT(*) as qtd')->fetchObject()->qtd;
+                //
+        //RESULTADOS DA PÁGINA
+        $results = EntityComments::getComment($where.' = '.$idUsuario,$filter.' '.$order);
+
+        //RENDERIZA ITEM
+        while($objComment = $results->fetchObject(EntityComments::class)){
+        
+            $itens [] = [
+                'idComment'           => (int)$objComment->idComment,
+                'idApp'               => (int)$objComment->idApp,
+                'idUsuario'           => (int)$objComment->idUsuario,
+                'estabelecimento'     => $objComment->estabelecimento,
+                'nome'                => $objComment->nome,
+                'comentario'          => $objComment->comentario,
+                'utilSim'             => (int)$objComment->utilSim,
+                'utilNao'             => (int)$objComment->utilNao,
+                'data'                => $objComment->data,
+                'avaliacao'           => (int)$objComment->avaliacao,
+                'custo'               => (int)$objComment->custo,
+            ];
+            
+        }
+
+        //RETORNA OS DEPOIMENTOS
+        return $itens;
+    }
+
+    /**
+     * Retorna todos os comentários de um usuário 
+     *
+     * @param Request $request
+     * @return array
+     */
+    public static function getCommentApp($request){
+
+
+        if(!is_numeric($request->user->idUsuario)){
+            
+            throw new \Exception("O id ".$request->user->idUsuario." não e valido", 400);
+
+        }
+
+        $objComment = EntityComments::getCommentByIdUser($request->user->idUsuario);
+
+        if(!$objComment instanceof EntityComments){
+            throw new \Exception("Não há comentários para o id: ".$request->user->idUsuario.".", 404);
+        }
+
+        return [
+            'comments'      => self::getAllCommentApp($request,$request->user->idUsuario,$objPagination,'idUsuario'),
+            'paginacao' => parent::getPagination($request,$objPagination)
+             
+        ];
+    }
+
+
+     /**
+     * Retorna todos os comentários de um usuário sem paginação 
+     *
+     * @param Request $request
+     * @return array
+     */
+    public static function getCommentAppAll($request){
+
+
+        if(!is_numeric($request->user->idUsuario)){
+            
+            throw new \Exception("O id ".$request->user->idUsuario." não e valido", 400);
+
+        }
+
+        $objComment = EntityComments::getCommentByIdUser($request->user->idUsuario);
+
+        if(!$objComment instanceof EntityComments){
+            throw new \Exception("Não há comentários para o id: ".$request->user->idUsuario.".", 404);
+        }
+
+        return [
+            'comments'      => self::getAllCommentAppAll($request,$request->user->idUsuario,'idUsuario'),
+             
+        ];
+    }
+   
+    /**
+     * Atualiza um comentário a um app
+     *
+     * @param Request $request
+     * @return array
+     */
+    public static function setEditCommentApp($request,$idComment){
+
+
+        $postVars = $request->getPostVars();
+
+        if(!isset($postVars['idApp']) || !isset($postVars['comentario']) || !isset($postVars['avaliacao']) ){
+
+            throw new \Exception("Os campos idApp, comentario e avaliação são obrigatórios", 400);
+            
+        }
+
+        $objComment = EntityComments::getCommentByIdAppComment($idComment);
+
+        if(!$objComment instanceof EntityComments){
+            throw new \Exception("Não há comentários para o id: ".$idComment.".", 404);
+        }
+        
+        if($objComment->idUsuario != $request->user->idUsuario){
+    
+            throw new \Exception("O comentário não pode ser editado por outra usuário", 404);
+
+        }
+
+        $validate = new Validate();
+
+        $words = Help::helpTextForArray($postVars['comentario']);
+       
+        if(!$validate->validateBlockedWord($words)){
+            throw new \Exception("Existe palavras impróprias no coméntario", 404);
+        }
+
+        $objApp = EntityApps::getAppById($postVars['idApp']);
+         
+        $objComment->idApp           = $postVars['idApp'];
+        $objComment->idUsuario       = $request->user->idUsuario;
+        $objComment->nome            = $request->user->nomeUsuario." ". $request->user->sobreNome;
+        $objComment->comentario      = $postVars['comentario'];
+        $objComment->custo           = $postVars['custo'];
+
+        if( $objComment->avaliacao != $postVars['avaliacao']){
+            $objApp = EntityApps::getAppById($postVars['idApp']);
+            $objApp->avaliacao       = $objApp->avaliacao + 1 ;
+            $objApp->totalAvaliacao  = $objApp->totalAvaliacao + $postVars['avaliacao'];
+            $objApp->totalCusto      = $objApp->totalCusto + $postVars['custo'];
+            $objApp->custoMedio      =  (float)$objApp->totalCusto / (float)$objApp->avaliacao;
+            $objApp->estrelas        =  (float)$objApp->totalAvaliacao / (float)$objApp->avaliacao;
+            $objApp->updateApp();
+        }
+       
+        //Atualiza o comentário
+        $objComment->updateComment();
+
+        //Retorna o commentario atualizado
+        return [
+                'idComment'        => (int)$objComment->idComment,
+                'idApp'            => (int)$objComment->idApp,
+                'estabelecimento'   => $objComment->estabelecimento,
+                'idUsuario'         => $objComment->idUsuario,
+                'nome'              => $objComment->nome,
+                'comentario'        => $objComment->comentario,
+                'utilSim'           => (int)$objComment->utilSim,
+                'utilNao'           => (int)$objComment->utilNao,
+                'data'              => $objComment->data,
+                'avaliacao'         => (int)$objComment->avaliacao,
+                'custo'             => (int)$objComment->custo,
+        ];
+
+    }
+    /**
+     * Excuir um comentário a um app
+     *
+     * @param Request $request
+     * @return array
+     */
+    public static function setDeleteCommentApp($request,$idComment){
+
+
+        $postVars = $request->getPostVars();
+        
+        $objComment = EntityComments::getCommentByIdAppComment($idComment);
+
+        if(!$objComment instanceof EntityComments){
+            throw new \Exception("Não há comentários para o id: ".$idComment.".", 404);
+        }
+        
+        if($objComment->idUsuario != $request->user->idUsuario){
+    
+            throw new \Exception("O comentário não pode ser excluído por outro usuário", 404);
+        }
+
+        //deleta o comentário
+        $objComment->deleteComment();
+
+        //Retorna o sucesso da exclusão
+        return [
+            'success'  => true,  
+        ];
+
+    }
+   
+}
